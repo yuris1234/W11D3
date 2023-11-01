b6af8964f4fd7d4c118473cdb9964b1c5146ed7c
Naoko Sugawara <infoseek122@gmail.com>
2023-10-31 21:11:47 -0400

    added produceReducer

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -0,0 +1,24 @@
+import produceData from '../mockData/produce.json';
+
+export default function produceReducer(state = {}, action) {
+    switch (action.type) {
+        case POPULATE:
+            const newState = {};
+            action.produce.forEach(produce => {
+              newState[produce.id] = produce;
+            });
+            return newState;
+        default:
+            return state;
+    }
+  }
+
+  const POPULATE = "produce/POPULATE";
+
+function populateProduce(){
+    return({
+        type: POPULATE,
+        produce: produceData
+    })
+}
+  
